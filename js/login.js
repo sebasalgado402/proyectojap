33a2f39ee4bb687d591e7b905abc8ceeb40de111
sebasalgado402 <sebasalgado402@gmail.com>
2021-08-20 01:18:57 -0300

    Muestra si tenes error al no poner un dato en el login

--- js/login.js
+++ js/login.js
@@ -14,43 +14,49 @@ function guardar(txtNombre, txtPassword){
     if (txtNombre.value==="" || txtPassword.value===""){ //Chequea que el dato recibido no esté vacío. 
     //El método trim elimina los espacios en blanco al inicio y al final del mismo.
         
-        verErrores();
-    }    else{
+        verErrores(txtNombre,txtPassword);
+
+    }else{
     localStorage.setItem("usuario", txtNombre.value); //setItem almacena el dato en la posición "usuario"
     localStorage.setItem("password", txtPassword.value); // Almaceno la contraseña
     sessionStorage.setItem("usuario", txtNombre.value);
-    
-    debugger;
-    
-    
-    
-    
+    redireccionar();
     //location.href="index.html";
     
     //getItem obtiene el dato almacenado en la posición "usuario"
-   
     }
 }
 
 
 function redireccionar(){
-    let user = document.getElementById('txtNombre');
-    let passwd = document.getElementById('txtPassword');
-    
     window.location="index.html"
 }
 
-function verErrores(){
-    if ((user.value == '') && (passwd.value == '')){    
-        let elementoNombre = document.getElementById('errorNombre');
+function verErrores(user,passwd){
+    let elementoContra = document.getElementById('errorPassword');
+    let elementoNombre = document.getElementById('errorNombre');
+    if(user.value == ''){
         elementoNombre.innerHTML = "Debe ingresar un nombre";
-        let elementoContra = document.getElementById('errorPassword');
-        elementoContra.innerHTML = "Debe ingresar una contraseña"; 
     }else{
-        elementoNombre.style.display = "none";
-        elementoContra.style.display = "none";
+        elementoNombre.style.display= "none";
+    }
+    if(passwd.value == ''){
+        
+        elementoContra.innerHTML = "Debe ingresar una contraseña"; 
+    }
+    else{
+        elementoContra.style.display= "none";
     }
 }
+
+document.getElementById('txtNombre').addEventListener('focus',function(){
+    let elementoNombre = document.getElementById('errorNombre');
+        elementoNombre.style.display= "none";
+});
+document.getElementById('txtPassword').addEventListener('focus',function(){
+    let elementoContra = document.getElementById('errorPassword');
+        elementoContra.style.display= "none";
+});
 
 
 
